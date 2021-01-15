ffd6dda633fd87b646fdec25c0c040932b6290cc
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-15 13:02:22 +0100

    node jest job

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
-const sum = require('./sum');
-const test = ('adds 1 + 2 to equal 3', () => {
+const sum = require("./sum");
+const test = ("adds 1 + 2 to equal 3", () => {
   const expect = (sum(1, 2)).toBe(3);
   console.log(expect)
 });
