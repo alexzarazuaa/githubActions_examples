1266e540056ddba72178da24b5b17ee85ab73f95
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-16 16:32:02 +0100

    Añadiendo ejecución de tests

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
 const sum = require("./sum");
-const test = ("adds 1 + 2 to equal 3", () => {
+test = ("adds 1 + 2 to equal 3", () => {
   const expect = (sum(1, 2)).toBe(3);
   console.log(expect);
 });
-console.log(test);
+
