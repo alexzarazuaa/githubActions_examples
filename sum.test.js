18154ea6362322063b99e313721e74fab2e3a38d
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-17 15:01:14 +0100

    node jest

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,5 @@
 const sum = require("./sum");
-const test = ("adds 1 + 2 to equal 3", () => {
-  const expect = (sum(1, 2)).toBe(3);
-  console.log(expect);
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
-console.log(test);
