10247cca2ecd21f97d69bc0d36ac3204585528db
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-28 11:29:03 +0100

    send_telegram_action

--- .github/actions/send_telegram_action/index.js
+++ .github/actions/send_telegram_action/index.js
@@ -1,9 +1,9 @@
 const core = require("@actions/core");
-
 const TelegramBot = require('node-telegram-bot-api');
+
 const token = core.getInput("token_father");
 const bot = new TelegramBot(token, {polling: false});
-var chat_Id=core.getInput("cahtId");
+var chat_Id=core.getInput("chatId");
 const name = core.getInput("name");
 
 
